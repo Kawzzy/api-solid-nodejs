4631df063cad5f4cab722fd23b86f5fa1c5dd5ea
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-09-16 00:07:23 -0300

    Prisma ORM initial config.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 import fastify from 'fastify'
 
+import { PrismaClient } from '@prisma/client'
+
 export const app = fastify()
+
+// instantiates the connection with the database
+const prisma = new PrismaClient()
