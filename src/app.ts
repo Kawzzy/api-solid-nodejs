9e91e7df02569b19ce75aa912aebeadce196388b
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-09-17 15:36:52 -0300

    Create user's route.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
 import fastify from 'fastify'
 
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-// instantiates the connection with the database
-const prisma = new PrismaClient()
+const userSchema = z.object({
+	name: z.string(),
+	email: z.string().email(),
+	password: z.string().min(6)
+})
+
+app.post('/users', async (req, res) => {
+	const { name, email, password } = userSchema.parse(req.body)
+
+	await prisma.user.create({
+		data: {
+			name,
+			email,
+			passwordHash: password
+		}
+	})
+
+	return res.status(201).send()
+})
