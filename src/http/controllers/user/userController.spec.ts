6c031952ad6a6cef2bee605316a9f0de6309d460
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-10-13 17:21:54 -0300

    User register e2e test.

--- src/http/controllers/user/userController.spec.ts
+++ src/http/controllers/user/userController.spec.ts
@@ -1,5 +1,24 @@
-import { test } from 'vitest'
+import request from 'supertest'
 
-test('ok', async () => {
-  
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('User Controller (e2e)', () => {
+	beforeAll(async () => {
+		await app.ready()
+	})
+
+	afterAll(async () => {
+		await app.close()
+	})
+
+	it('should create user', async () => {
+		const response = await request(app.server).post('/users').send({
+			name: 'User',
+			email: 'userTest@gmail.com',
+			password: '123456'
+		})
+
+		expect(response.statusCode).toEqual(201)
+	})
 })
