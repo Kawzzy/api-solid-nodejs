9b76781ef32874333dbc7c0a341a896455beea68
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-10-13 14:28:25 -0300

    getUserProfile controller and middleware to validate the jwt.

--- src/http/controllers/user/profileController.ts
+++ src/http/controllers/user/profileController.ts
@@ -1,9 +1,19 @@
+import { GetUserProfileServiceFactory } from '@/services/user/factories/getUserProfileServiceFactory'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profileController(req: FastifyRequest, res: FastifyReply) {
 	await req.jwtVerify()
 
-	console.log(req.user.sub)
+	const getUserProfile = GetUserProfileServiceFactory()
+
+	const { user } = await getUserProfile.execute({
+		userId: req.user.sub
+	})
   
-	return res.status(200).send()
+	return res.status(200).send({
+		user: {
+			...user,
+			passwordHash: undefined
+		}
+	})
 }
--- src/http/routes/user/profileRoutes.ts
+++ src/http/routes/user/profileRoutes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '@/http/middlewares/verifyJWT'
 import { profileController } from '@/http/controllers/user/profileController'
 
 export async function profileRoutes(app: FastifyInstance) {
-	app.get('/', profileController)
+	app.get('/', { onRequest: [verifyJWT] }, profileController)
 }
