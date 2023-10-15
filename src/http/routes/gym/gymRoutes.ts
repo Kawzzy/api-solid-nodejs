1386fe2890479bc2d8e922f85ce8a77453dace81
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-10-15 18:04:08 -0300

    Create gym's route.

--- src/http/routes/gym/gymRoutes.ts
+++ src/http/routes/gym/gymRoutes.ts
@@ -1,11 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verifyJWT'
+import { GymController } from '@/http/controllers/gym/gymController'
 import { SearchGymController } from '@/http/controllers/gym/searchGymController'
 import { FetchNearbyGymController } from '@/http/controllers/gym/fetchNearbyGymController'
 
 export async function gymRoutes(app: FastifyInstance) {
 	app.addHook('onRequest', verifyJWT)
-
+  
+	app.get('/create', GymController)
 	app.get('/search', SearchGymController)
 	app.get('/nearby', FetchNearbyGymController)
 }
